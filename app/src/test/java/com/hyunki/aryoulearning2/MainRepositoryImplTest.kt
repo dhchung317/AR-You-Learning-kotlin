83b73e587af20aeb206cb5e5707934ca349908f9
hyunki <dhchung317@gmail.com>
2020-06-06 23:17:29 -0400

    added couple tests for repo get calls

--- app/src/test/java/com/hyunki/aryoulearning2/MainRepositoryImplTest.kt
+++ app/src/test/java/com/hyunki/aryoulearning2/MainRepositoryImplTest.kt
@@ -4,20 +4,36 @@ import android.content.Context
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.hyunki.aryoulearning2.data.MainRepositoryImpl
 import com.hyunki.aryoulearning2.data.db.ModelDatabase
+import com.hyunki.aryoulearning2.data.db.dao.CategoryDao
+import com.hyunki.aryoulearning2.data.db.model.Category
+import com.hyunki.aryoulearning2.data.network.main.MainApi
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.spy
+import org.apache.tools.ant.Main
+import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Rule
+import org.junit.Test
 import org.junit.rules.ExpectedException
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
 
+@RunWith(AndroidJUnit4::class)
 class MainRepositoryImplTest {
 
     @get:Rule
     val testRule = InstantTaskExecutorRule()
 
     @get:Rule
-    val excRule = ExpectedException.none()
+    val expectedExceptionRule: ExpectedException = ExpectedException.none()
 
     private lateinit var db: ModelDatabase
+//    private lateinit var spyCatDao: CategoryDao
+//    private lateinit var spyModelDao: CategoryDao
+//    private lateinit var mainApi: MainApi
 
     @Before
     fun setup() {
@@ -27,4 +43,38 @@ class MainRepositoryImplTest {
                 .allowMainThreadQueries()
                 .build()
     }
+
+    @Test
+    fun `assert getAllCats() returns size zero when empty`() {
+        val modelDao = spy(db.modelDao())
+        val catDao = spy(db.catDao())
+        val api = mock<MainApi>()
+        val repo = MainRepositoryImpl(modelDao,catDao, api)
+
+        val expected = 0
+
+        val actual = repo.getAllCats().blockingGet().size
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `assert getAllCats() returns list of 3 categories when 3 categories populated`() {
+        val modelDao = spy(db.modelDao())
+        val catDao = spy(db.catDao())
+        val api = mock<MainApi>()
+
+        db.catDao().insert(Category("category1", "image1"))
+        db.catDao().insert(Category("category2", "image2"))
+        db.catDao().insert(Category("category3", "image3"))
+
+        val repo = MainRepositoryImpl(modelDao,catDao, api)
+
+        val expected = 3
+
+        val actual = repo.getAllCats().blockingGet().size
+
+        assertEquals(expected, actual)
+    }
+
 }
