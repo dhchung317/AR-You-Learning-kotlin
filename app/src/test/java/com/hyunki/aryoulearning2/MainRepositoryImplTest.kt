64bbb87e0b0f4a94e48e12ac9b42919b4e59be75
hyunki <dhchung317@gmail.com>
2020-06-06 23:26:12 -0400

    test for insert cat method

--- app/src/test/java/com/hyunki/aryoulearning2/MainRepositoryImplTest.kt
+++ app/src/test/java/com/hyunki/aryoulearning2/MainRepositoryImplTest.kt
@@ -8,11 +8,15 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.hyunki.aryoulearning2.data.MainRepositoryImpl
 import com.hyunki.aryoulearning2.data.db.ModelDatabase
 import com.hyunki.aryoulearning2.data.db.dao.CategoryDao
+import com.hyunki.aryoulearning2.data.db.dao.ModelDao
 import com.hyunki.aryoulearning2.data.db.model.Category
 import com.hyunki.aryoulearning2.data.network.main.MainApi
+import com.nhaarman.mockitokotlin2.argumentCaptor
 import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.spy
+import com.nhaarman.mockitokotlin2.verify
 import org.apache.tools.ant.Main
+import org.junit.After
 import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Rule
@@ -46,10 +50,10 @@ class MainRepositoryImplTest {
 
     @Test
     fun `assert getAllCats() returns size zero when empty`() {
-        val modelDao = spy(db.modelDao())
+        val modelDao = mock<ModelDao>()
         val catDao = spy(db.catDao())
         val api = mock<MainApi>()
-        val repo = MainRepositoryImpl(modelDao,catDao, api)
+        val repo = MainRepositoryImpl(modelDao, catDao, api)
 
         val expected = 0
 
@@ -60,7 +64,7 @@ class MainRepositoryImplTest {
 
     @Test
     fun `assert getAllCats() returns list of 3 categories when 3 categories populated`() {
-        val modelDao = spy(db.modelDao())
+        val modelDao = mock<ModelDao>()
         val catDao = spy(db.catDao())
         val api = mock<MainApi>()
 
@@ -68,7 +72,7 @@ class MainRepositoryImplTest {
         db.catDao().insert(Category("category2", "image2"))
         db.catDao().insert(Category("category3", "image3"))
 
-        val repo = MainRepositoryImpl(modelDao,catDao, api)
+        val repo = MainRepositoryImpl(modelDao, catDao, api)
 
         val expected = 3
 
@@ -77,4 +81,27 @@ class MainRepositoryImplTest {
         assertEquals(expected, actual)
     }
 
+    @Test
+    fun `verify insertCat() inserts expected item`() {
+        val modelDao = mock<ModelDao>()
+        val catDao = mock<CategoryDao>()
+        val api = mock<MainApi>()
+
+        val expected = Category("category1", "image1")
+
+        val repo = MainRepositoryImpl(modelDao, catDao, api)
+
+        repo.insertCat(expected)
+
+        argumentCaptor<Category>()
+                .apply {
+                    verify(catDao).insert(capture())
+                    assertEquals(expected, firstValue)
+                }
+    }
+
+    @After
+    fun teardown() {
+        db.close()
+    }
 }
